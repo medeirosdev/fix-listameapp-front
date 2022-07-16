2f0a68fc07307535081f3dc295046b39b8708314
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-07-16 16:47:40 -0300

    refactor: removed unused code

--- src/app/navigations/index.tsx
+++ src/app/navigations/index.tsx
@@ -1,32 +1,14 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import RNBootSplash from 'react-native-bootsplash';
 import { NavigationContainer } from '@react-navigation/native';
-import { PublicNavigator } from '~/app/navigations/public/PublicNavigator';
 import { PrivateNavigator } from '~/app/navigations/private/PrivateNavigator';
-import { SplashScreen } from '~/modules/auth/screens/SplashScreen';
 
 export const delay = (ms: number) =>
   new Promise((resolve) => setTimeout(() => resolve(true), ms));
 
 export const Navigations: FC = () => {
-  const [isLogged, setIsLogged] = useState(false);
-
-  useEffect(() => {
-    // delay(6000).then(() => setIsLogged(true));
-  }, []);
-
-  useEffect(() => {
-    if (isLogged) {
-    }
-  }, [isLogged]);
-
-  // if (!isLogged) {
-  //   return <SplashScreen />;
-  // }
-
   return (
-    <NavigationContainer>
-      {/* <NavigationContainer onReady={() => RNBootSplash.hide({ fade: true })}> */}
+    <NavigationContainer onReady={() => RNBootSplash.hide({ fade: true })}>
       <PrivateNavigator />
     </NavigationContainer>
   );
