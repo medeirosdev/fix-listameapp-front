8162911d525dfcb55af4d49f44039a1342f1361b
medeirosdev <guilherme.ggcm20@gmail.com>
2023-07-01 12:45:15 -0300

    :sparkles:

--- src/modules/auth/services/api/userApi.ts
+++ src/modules/auth/services/api/userApi.ts
@@ -1,5 +1,5 @@
 import { api } from '~/app/services/api/api';
-
+import axios from 'axios';
 import {
   ICreateUserRequest,
   ICreateUserResponse,
@@ -12,9 +12,32 @@ interface ICreateUser {
 const BASE_URL = '/users';
 
 const createUser: ICreateUser = async (userData) => {
+
+/*
+  const testdata =  {
+    "name" : "gu",
+    "email": "1emailsuper@gmail.com",
+    "login" : "1dev2m213123edei2ros22" ,  
+    "password" : "aa123456"
+}
+  console.log(testdata)
+  try{
+    const {data} = await axios.post('http://localhost:3333/users/', testdata);
+    return data
+  }catch(e:any){
+    console.log(e)
+  }
+  */
+  
   const endpoint = BASE_URL.concat('/');
+  console.log("==== Userdata do Registro aqui! ========================")
+  console.log(userData)
+  console.log(endpoint)
   const { data } = await api.post(endpoint, userData);
+  console.log("==== API AXIOS DATA ========================")
+  console.log(data)
   return data;
+  
 };
 
 export const userApi = {
